7b0dde637547890059b9f16afe7d6b3896423017
陶飞 <541003193@qq.com>
2022-01-17 16:03:31 +0800

    添加ArrayDiff

--- php.go
+++ php.go
@@ -1112,6 +1112,34 @@ func ArrayMerge(ss ...[]interface{}) []interface{} {
 	return s
 }
 
+// array_diff()
+func ArrayDiff(array1 []string, arrayOthers ...[]string) []string {
+	c := make(map[string]bool)
+	for i := 0; i < len(array1); i++ {
+		if _, hasKey := c[array1[i]]; hasKey {
+			c[array1[i]] = true
+		} else {
+			c[array1[i]] = false
+		}
+	}
+	for i := 0; i < len(arrayOthers); i++ {
+		for j := 0; j < len(arrayOthers[i]); j++ {
+			if _, hasKey := c[arrayOthers[i][j]]; hasKey {
+				c[arrayOthers[i][j]] = true
+			} else {
+				c[arrayOthers[i][j]] = false
+			}
+		}
+	}
+	result := make([]string, 0)
+	for k, v := range c {
+		if !v {
+			result = append(result, k)
+		}
+	}
+	return result
+}
+
 // ArrayChunk array_chunk()
 func ArrayChunk(s []interface{}, size int) [][]interface{} {
 	if size < 1 {
